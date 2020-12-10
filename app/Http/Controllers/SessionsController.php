a9d0a591c7802664ac213b1757c5be44439773c7
徐大帅 <fibonacci701408733@163.com>
2020-12-10 16:30:27 +0800

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -13,11 +13,11 @@ class SessionsController extends Controller
     }
 
     public function store(Request $request){
-        $this->validate($request, [
+        $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
             'password' => 'required',
         ]);
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+        if(Auth::attempt($credentials, $request->has('remember'))){
             session()->flash('success', '欢迎回来~');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
