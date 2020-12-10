d35f0e44146b92f729c9bc426e0887ebc35a3e18
徐大帅 <fibonacci701408733@163.com>
2020-12-10 13:55:59 +0800

    移动User.php到app/Models下

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,11 +10,12 @@ class CreateUsersTable extends Migration
      * Run the migrations.
      *
      * @return void
+     *
      */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
@@ -28,6 +29,7 @@ class CreateUsersTable extends Migration
      * Reverse the migrations.
      *
      * @return void
+     * 回滚
      */
     public function down()
     {
