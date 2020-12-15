c7d6c677224abd3b80941313d544496e16b0921d
徐大帅 <fibonacci701408733@163.com>
2020-12-15 10:34:20 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    // 允许被更新的字段
+    protected $fillable = ['content'];
     //一条博文只能属于一个用户
     public function user(){
         return $this->belongsTo(User::class);
